7c2d228a7e508e0689a5754c5484efdfdd5e8c86
Victor Matheus <victor.id96@hotmail.com>
2022-06-20 17:58:18 -0300

    Inclusão da verificação de erro no jogo

--- js/main.js
+++ js/main.js
@@ -12,27 +12,42 @@ let getBtn = document.querySelector("#getPokemon");
 let img = document.querySelector('#pokemon-image');
 let options = document.querySelector('.options');
 let pokemonName = document.querySelector('.pokemon-name');
+let imageContainer = document.querySelector('.image-container');
 window.addEventListener('load', updatePokemons);
 getBtn === null || getBtn === void 0 ? void 0 : getBtn.addEventListener('click', updatePokemons);
 options === null || options === void 0 ? void 0 : options.addEventListener('click', (event) => {
     let element;
     element = event.target;
+    if (element.textContent == `Play again!`)
+        return;
     if (element.innerText == img.dataset.name) {
         img.classList.remove('pokemon-hidden');
         img.classList.add('pokemon-visible');
         pokemonName.classList.add('visible');
         pokemonName.textContent = `Its's ${element.innerText}!`;
-        options.innerHTML = '';
-        options.append(createButton(`Play again!`, updatePokemons));
-        options.append(createButton(`See in Pokedex`));
+        finishGame(true);
+    }
+    else {
+        img.classList.add('pokemon-disappear');
+        pokemonName.classList.add('visible-error');
+        pokemonName.textContent = `Oh no! :(`;
+        finishGame();
     }
 });
+function finishGame(win = false) {
+    options.innerHTML = '';
+    options.append(createButton(`Play again!`, updatePokemons));
+    if (win)
+        options.append(createButton(`See in Pokedex`));
+}
 function updatePokemons() {
     return __awaiter(this, void 0, void 0, function* () {
+        img.className = '';
+        pokemonName.className = '';
         options.innerHTML = '';
         pokemonName.classList.remove(`visible`);
         let selected;
-        selected = randomBeetwen(1, 3);
+        selected = randomBeetwen(0, 3);
         for (let index = 0; index <= 2; index++) {
             let pokemon;
             pokemon = yield getPokemon();
